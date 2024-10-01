156934d266fbbcd2dc2185bd16cbf3b70896086d
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-01 15:42:12 +0200

    add FizzBuzz for multiple of 3 and 5

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ console.log('linked');
 
 for(let i = 1; i <= 100; i++) {
 
-    if(i % 3 == 0){
-        console.log('Fizz');
+    if(i % 5 == 0 && i % 3 == 0){
+        console.log('FizzBuzz');
         
     } else if (i % 5 == 0){
         console.log('Buzz');
+    
+    
+    }else if (i % 3 == 0){
+        console.log('Fizz');
     
     
     }else{
